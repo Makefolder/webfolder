c0d1eb04a5ed59da96571b084161b60838478ed6
Artemii <artemii.fedotov@tutamail.com>
2025-07-01 23:28:50 +0200

    structure changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,12 @@ import PageConsts from './lib/consts';
 import LeftPart from './ui-kit/LeftPart';
 import RightPart from './ui-kit/RightPart';
 
-import ellipse from './assets/Ellipse.svg';
-
 export default function App() {
   const { name, subtitle, slogan, navLinks, socials, aboutMe, sections } =
     PageConsts;
   return (
     <div>
       <div class="bg-noise"></div>
-{/*       <div class="fixed -top-[10rem] -right-[20rem] -z-10">
-        <img src={ellipse} alt="ellipse" />
-      </div> */}
       <main class="flex justify-between flex-col items-center lg:flex-row lg:items-start min-h-screen max-w-[1174px] mx-auto px-[10px]">
         <LeftPart
           name={name}
